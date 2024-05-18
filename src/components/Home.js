48a4a2e80ba30b826f21daf50d47b8d2148b51f2
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-18 18:13:15 +0300

    Update the components with jsx syntax

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <nav id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </nav>
+  );
 }
 
 export default Home;
